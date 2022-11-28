8b468b03e3aa5fc2221ee89e07612b92e17c6450
Daniel Skysľak <danilesky17@gmail.com>
2022-11-28 23:30:05 +0100

    loading data

--- src/components/AppTable.jsx
+++ src/components/AppTable.jsx
@@ -8,51 +8,55 @@ import { IconButton, styled } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import { LoadingButton } from "@mui/lab";
 
 export const AppTable = ({ data, deleteFn, editFn, completeFn }) => {
   return (
-    <TableContainer>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Task</TableCell>
-            <TableCell align="right">Actions</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data &&
-            data.map((row) => (
-              <TableRow key={row.name}>
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="right">
-                  <IconButton
-                    aria-label="edit"
-                    color="primary"
-                    onClick={editFn}
-                  >
-                    <EditIcon />
-                  </IconButton>
-                  <IconButton
-                    aria-label="delete"
-                    color="primary"
-                    onClick={deleteFn}
-                  >
-                    <DeleteIcon />
-                  </IconButton>
-                  <IconButton
-                    aria-label="complete task"
-                    color="primary"
-                    onClick={completeFn}
-                  >
-                    <CheckCircleIcon />
-                  </IconButton>
-                </TableCell>
-              </TableRow>
-            ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      <TableContainer>
+        <Table sx={{ minWidth: 700 }} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Task</TableCell>
+              <TableCell align="right">Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data &&
+              data.map((row) => (
+                <TableRow key={row.name}>
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">
+                    <IconButton
+                      aria-label="edit"
+                      color="primary"
+                      onClick={editFn}
+                    >
+                      <EditIcon />
+                    </IconButton>
+                    <IconButton
+                      aria-label="delete"
+                      color="primary"
+                      onClick={deleteFn}
+                    >
+                      <DeleteIcon />
+                    </IconButton>
+                    <IconButton
+                      aria-label="complete task"
+                      color="primary"
+                      onClick={completeFn}
+                    >
+                      <CheckCircleIcon />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      {!data.length > 0 && <LoadingButton loading>Submit</LoadingButton>}
+    </>
   );
 };
