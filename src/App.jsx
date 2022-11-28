14bc17eafea0d9669395f5980c7fcc1f4219e4cc
Daniel Skysľak <danilesky17@gmail.com>
2022-11-28 22:52:08 +0100

    Todo Table component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
+import Box from "@mui/material/Box";
+import { AppTable } from "./components/AppTable";
+import { styled } from "@mui/material";
+
+const AppBody = styled(Box)`
+  height: 100%;
+  width: 100%;
+  background-color: white;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+function createData(name, description) {
+  return { name, description, completed: false };
+}
+
+const rows = [
+  createData("Clean", "Zajr povysávať"),
+  createData("Upratovanie", "Zajr povysávať"),
+  createData("Cvicit", "Zajr povysávať"),
+];
+
 function App() {
-  return <></>;
+  return (
+    <AppBody>
+      <AppTable data={rows} />
+    </AppBody>
+  );
 }
 
 export default App;
---
+++ src/components/AppTable.jsx
@@ -0,0 +1,46 @@
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import { IconButton, styled } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+
+export const AppTable = ({ data }) => {
+  return (
+    <TableContainer>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Task</TableCell>
+            <TableCell align="right">Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data &&
+            data.map((row) => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">
+                  <IconButton aria-label="edit" color="primary">
+                    <EditIcon />
+                  </IconButton>
+                  <IconButton aria-label="delete" color="primary">
+                    <DeleteIcon />
+                  </IconButton>
+                  <IconButton aria-label="complete task" color="primary">
+                    <CheckCircleIcon />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
